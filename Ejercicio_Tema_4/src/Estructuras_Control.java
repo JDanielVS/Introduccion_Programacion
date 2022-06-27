d95b036ed3a8f86315fae72db651b4077c980b32
JDanielVS <jdanielvsan@gmail.com>
2022-06-27 21:34:37 +0200

    Ejercicio 4 Terminado

--- Ejercicio_Tema_4/src/Estructuras_Control.java
+++ Ejercicio_Tema_4/src/Estructuras_Control.java
@@ -7,6 +7,7 @@ public class Estructuras_Control {
         Scanner sc = new Scanner(System.in);
         int numeroIf = sc.nextInt();
 
+        //Creamos un if que compare y nos diga si es positivo, negativo o 0
         if(numeroIf > 0){
             System.out.println("El número es positivo");
         }else if(numeroIf < 0){
@@ -18,6 +19,7 @@ public class Estructuras_Control {
         System.out.println("Introduce otro número");
         int numeroWhile = sc.nextInt();
 
+        //Creamos un while con la condición <3 y aumentará en 1 cada vez el número
         while (numeroWhile < 3){
             numeroWhile++;
             System.out.println("Tu número incrementado vale " + numeroWhile);
@@ -26,12 +28,14 @@ public class Estructuras_Control {
         System.out.println("Introduce otro número");
         int numeroWhile2 = sc.nextInt();
 
+        //Creamos un doWhile con la misma condición anterior pero solo se ejecutará una vez
         do {
             numeroWhile2++;
             System.out.println("Tu número incrementado vale " + numeroWhile2);
             break;
         }while (numeroWhile2<3);
 
+        //Un bucle for, cuyo valor es 0 y aumenta el número hasta 3
         for (int numeroFor = 0; numeroFor <= 3; numeroFor++){
             System.out.println("Tu número vale " + numeroFor);
         }
@@ -39,6 +43,7 @@ public class Estructuras_Control {
         System.out.println("Introduce una estación");
         String estacion = sc.next();
 
+        //Por último un swith que nos diga la estación que le digamos nosotros y su default
         switch (estacion.toLowerCase()){
             case "otoño":
                 System.out.println("Estamos en otoño");
